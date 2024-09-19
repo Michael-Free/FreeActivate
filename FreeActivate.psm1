06f1da96076063827be917b9b0c7bdc2dd2ce234
Michael-Free <michael.d.free@gmail.com>
2024-09-18 21:58:55 -0400

    Adding in global vars for IP/FQDN/WinKey Regex

--- FreeActivate.psm1
+++ FreeActivate.psm1
@@ -1,4 +1,9 @@
 # FreeActivate.psm1
+
+$ipRegex = '^(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])$'
+$fqdnRegex = '^(?=.{1,255}$)([a-zA-Z0-9]+(-[a-zA-Z0-9]+)*\.)+[a-zA-Z]{2,}$'
+$windowsKeyRegex = '^[A-Za-z0-9]{5}-[A-Za-z0-9]{5}-[A-Za-z0-9]{5}-[A-Za-z0-9]{5}-[A-Za-z0-9]{5}$'
+
 function Get-Activation() {
   $licenseInfo = (cscript.exe /nologo C:\Windows\System32\slmgr.vbs /dlv) -Split "`n"
   $licenseStatus = $licenseInfo | ForEach-Object { 
@@ -29,7 +34,7 @@ function Set-KmsActivation() {
   }
 
   if ([string]::IsNullOrEmpty($Server) -or [string]::IsNullOrEmpty($Key)) {
-    throw "-Key or -Server flag cannot be empty or have a null value."
+    throw "-Key or -Server parameters cannot be empty or have a null value."
   }
 
 }
